6a63cd2d2d91c6db1a89213c00ec97478dac299f
Sriram MK <mksriram@umich.edu>
2024-01-15 22:36:58 -0500

    testing

---
+++ background.js
@@ -0,0 +1,9 @@
+// class:video-js
+const videoList = document.querySelectorAll("video") 
+
+for( let i = 0; i < videoList.length; i++ ){
+
+    const mainVideo = document.getElementsByTagName('video')
+    
+} 
+
